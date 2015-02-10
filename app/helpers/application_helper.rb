3833da0b389465138b7469e64d9639384bce6e0c
danielpump <new_daniel2010@hotmail.com>
2015-02-10 02:23:57 -0200

    Foi incluido o CSS simplex do bootstrap e o form de inserção começou a ser trabalhado, alguns componentes visuais foram criados para crianção de input text no form e de botão do form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
       content_tag( :div, model.errors[attribute].first, class: 'error_message' )
     end
   end
+
+  def form_text_input_field(model, attribute)
+    content_tag(:div, :class => "form-group") do
+      label_tag(attribute, nil, :class => "col-lg-2 control-label") <<
+      text_field_tag(attribute, nil, :class => "form-control") <<
+      error_tag(model, attribute)
+    end
+  end
+  
+  def form_submit_button()
+    content_tag(:div, :class => "form-group") do
+      #Ver como recuperar o nome da tela no botão
+      submit_tag(nil,:class => "btn btn-primary")
+    end
+  end
+    
+
 end
