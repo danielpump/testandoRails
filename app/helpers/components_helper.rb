dc477a5a4f62a822b2b279247d9e24ce5cd18234
danielpump <new_daniel2010@hotmail.com>
2015-02-14 17:52:19 -0200

    header do data table sendo gerado com sucesso, iniciado geração do body

--- app/helpers/components_helper.rb
+++ app/helpers/components_helper.rb
@@ -7,47 +7,38 @@ module ComponentsHelper
 
   def form_text_input_field(form, model, attribute)
     content_tag(:div, :class => "form-group") do
-      (form.label attribute, :class => "col-lg-full control-label") <<        
-      (form.text_field attribute, :class => "form-control") <<  
+      (form.label attribute, :class => "col-lg-full control-label") <<
+      (form.text_field attribute, :class => "form-control") <<
       (error_tag(model, attribute))
     end
   end
-  
+
   def form_submit_button(form)
     content_tag(:div, :class => "form-group") do
       (form.submit :class => "btn btn-default")
     end
   end
-    
+
   def data_table(models, attributes = [])
-      
-      content_tag(:table, :class => "table table-striped table-hover") do
-        content_tag(:thead) do
-          content_tag(:tr) do
-            attributes.each do |attribute|
-              content_tag(:th) do
-                #puts (">>>>>>>>>>>>> " << ("'.#{attribute}'")) 
-                #puts (">>>>>>>>>>>>> " << (t "'.#{attribute}'"))
-                #(t "'.#{attribute}'")
-                ("Teste")
-              end
-            end
-          end            
+
+    content_tag(:table, :class => "table table-striped table-hover") do
+     (content_tag(:thead) do
+        content_tag :tr do
+          attributes.collect { |attribute|
+            concat content_tag(:th, (t ".#{attribute}"))
+          }.to_s.html_safe
         end
-        content_tag(:tdoby) do
-          models.each do |model|        
-            content_tag(:tr) do
-              attributes.each do |attribute|
-                content_tag(:td) do
-                  puts model[attribute]
-                  model[attribute]
-                end
-              end            
-            end
-          end    
-        end
-      end
-      
-  end
+     end) <<
+     (content_tag :tbody do
+        models.collect { |model|
+          concat content_tag :tr do
+            attributes.collect { |attribute|
+             content_tag(:td, model[attribute])
+            }.to_s.html_safe  
+          end         
+        }.to_s.html_safe
+     end)
+   end
+ end
 
 end
